399413a123304f11c9da053e61e67e7809b2fe64
Nishanth P <nishanthpechimuthu@gmail.com>
2026-06-11 22:11:03 +0530

    SEM IV DONE!

--- Java/code/EX03/EvenOdd.java
+++
@@ -1,9 +0,0 @@
-class EvenOdd {
-public static void main(String[] args) {
-int n = 6;
-if (n % 2 == 0)
-System.out.println("Even Number");
-else
-System.out.println("Odd Number");
-}
-}
--- Java/code/EX03/PositiveNumber.java
+++
@@ -1,8 +0,0 @@
-class PositiveNumber {
-public static void main(String[] args) {
-int n = 5;
-if (n > 0) {
-System.out.println("Positive Number");
-}
-}
-}
